e787585563105c2a6f03c76d90bfc1e2563c6b52
jaideepghosh <er.jaideepghosh@gmail.com>
2020-10-08 22:27:37 +0530

    feat(translation):Updated the barrel.

--- packages/stateofjs/lib/i18n/index.js
+++ packages/stateofjs/lib/i18n/index.js
@@ -1,13 +1,21 @@
-import {default as enCommon} from './common/js/en-US.js';
-import {default as enCSS} from './state-of-css/js/en-US.js';
-import {default as enJS} from './state-of-js/js/en-US.js';
+import { default as enCommon } from "./common/js/en-US.js";
+import { default as enCSS } from "./state-of-css/js/en-US.js";
+import { default as enJS } from "./state-of-js/js/en-US.js";
+import { default as hiCommon } from "./common/js/hi-IN.js";
+import { default as hiCSS } from "./state-of-css/js/hi-IN.js";
+import { default as hiJS } from "./state-of-js/js/hi-IN.js";
 
 const locales = [
   {
-    id: 'en',
-    label: 'English',
-    stringFiles: [ enCommon, enCSS, enJS ]
-  }
+    id: "en",
+    label: "English",
+    stringFiles: [enCommon, enCSS, enJS],
+  },
+  {
+    id: "hi",
+    label: "Hindi",
+    stringFiles: [hiCommon, hiCSS, hiJS],
+  },
 ];
 
 export default locales;
