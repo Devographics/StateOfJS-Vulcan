3ffd90be79f24184e0e41cb733611c4fa182270e
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-26 14:10:04 +0900

    Add JS2020 outline

--- packages/stateofjs/lib/surveys/index.js
+++ packages/stateofjs/lib/surveys/index.js
@@ -1,13 +1,17 @@
 import js2019 from './js/js2019outline';
+
 import css2020 from './js/css2020outline';
+import js2020 from './js/js2020outline';
+
 import { parseSurvey } from '../modules/responses/helpers';
 
 // make sure array is properly sorted here
 const surveys = [
   css2020,
   js2019,
+  js2020,
 ];
 
 const parsedSurveys = surveys.map(parseSurvey);
 
-export default parsedSurveys
+export default parsedSurveys;
