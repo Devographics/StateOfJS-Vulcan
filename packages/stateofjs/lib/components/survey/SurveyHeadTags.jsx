1010efa25762697c3c5279016a52971e94a7f309
Sacha Greif <SachaG@users.noreply.github.com>
2021-09-28 14:52:02 +0900

    Add proper description

--- packages/stateofjs/lib/components/survey/SurveyHeadTags.jsx
+++ packages/stateofjs/lib/components/survey/SurveyHeadTags.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Components } from 'meteor/vulcan:core';
+import { intlShape } from 'meteor/vulcan:i18n';
 
-const SurveyHeadTags = ({ survey }) => {
+const SurveyHeadTags = ({ survey }, { intl }) => {
   const { name, year, imageUrl } = survey;
-  return <Components.HeadTags title={`${name} ${year}`} image={`/surveys/${imageUrl}`} />;
+  return (
+    <Components.HeadTags
+      title={`${name} ${year}`}
+      description={intl.formatMessage({ id: 'general.take_survey' }, { name, year })}
+      image={`/surveys/${imageUrl}`}
+    />
+  );
+};
+
+SurveyHeadTags.contextTypes = {
+  intl: intlShape,
 };
 
 export default SurveyHeadTags;
